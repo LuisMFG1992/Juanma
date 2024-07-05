707c930db5f34e21255cdc886f2c109e48e41fa9
LuisMFG <luismiguelfloresgr@gmail.com>
2024-07-05 23:33:39 +0200

    i18next implemented

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,21 @@
 import { Link } from 'react-router-dom'
 import AppButton from './AppButton'
+import { useTranslation } from 'react-i18next'
 
 const Hero = () => {
+  const { t, i18n } = useTranslation()
+
   return (
     <section className='bgHero py-28'>
       <div className='flex h-full flex-col items-center justify-center px-6 text-center sm:px-20 md:px-28 lg:px-52'>
         <h1 className='text-[3rem] font-medium text-white md:text-[4rem]'>
-          Unlock Full Potential
+          {t('heroTitle')}
         </h1>
         <p className='mb-8 text-paragraph leading-relaxed text-white'>
-          Traditional solar project optimizations are slow and ineffective. At
-          FIDgate, we offer intelligent solutions to change this. Discover how
-          our innovative service boost your project{"'"}s profitability and
-          enhance the decision-making process.
+          {t('heroParagraph')}
         </p>
         <Link to={'/services'} className='flex justify-center'>
-          <AppButton text={'Our Services'} hover={'lighter'} />
+          <AppButton text={t('heroButton')} hover={'lighter'} />
         </Link>
       </div>
     </section>
