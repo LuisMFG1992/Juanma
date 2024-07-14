eb279df807f934b73aba52163c08b53639b75d16
LuisMFG <luismiguelfloresgr@gmail.com>
2024-07-14 13:20:53 +0200

    cookies banner translated

--- src/components/CookiesBanner.jsx
+++ src/components/CookiesBanner.jsx
@@ -1,8 +1,11 @@
 import { useCookies } from 'react-cookie'
 import AppButton from './AppButton'
 import { Link } from 'react-router-dom'
+import { Trans, useTranslation } from 'react-i18next'
 
 const CookiesBanner = () => {
+  const { t } = useTranslation()
+
   const [cookies, setCookie] = useCookies(['cookieConsent'])
   const giveCookieConsent = () => {
     setCookie('cookieConsent', true, { path: '/' })
@@ -12,18 +15,21 @@ const CookiesBanner = () => {
     <section className='fixed bottom-0'>
       <div className='flex w-screen flex-col items-center justify-center gap-4 border-t border-primary bg-white px-6 py-4 md:flex-row md:gap-10'>
         <div className='text-center text-paragraph md:text-start'>
-          We use cookies to enhance your user experience. By using our website,
-          you agree to our use of cookies.{' '}
-          <Link
-            className='font-semibold text-primary underline '
-            to={'/privacyPolicy'}
-          >
-            Learn more.
-          </Link>
+          <Trans
+            i18nKey={'cookies_paragraph'}
+            components={{
+              1: (
+                <Link
+                  className='font-semibold text-primary underline '
+                  to={'/privacyPolicy'}
+                ></Link>
+              )
+            }}
+          />
         </div>
         <div className='flex gap-6'>
           <AppButton
-            text={'Agree'}
+            text={t('cookies_button')}
             hover='lighter'
             callBack={giveCookieConsent}
           />
