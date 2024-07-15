acb583acb970b692f60bd64e147c9908769a3739
LuisMFG <luismiguelfloresgr@gmail.com>
2024-07-15 19:57:13 +0200

    Languages done

--- src/components/HowWeWork.jsx
+++ src/components/HowWeWork.jsx
@@ -6,124 +6,111 @@ import { FaCheckDouble } from 'react-icons/fa'
 import { LiaSearchSolid } from 'react-icons/lia'
 import { FaRegHandshake } from 'react-icons/fa'
 import { BsDatabaseGear } from 'react-icons/bs'
+import { Trans, useTranslation } from 'react-i18next'
 
 const HowWeWork = () => {
+  const { t } = useTranslation()
+
   return (
     <section className='flex flex-col gap-8 px-2 py-24 sm:px-6'>
       <div className='flex flex-col gap-4'>
-        <h2 className='text-center text-subtitle font-medium text-primary'>
-          HOW WE WORK
+        <h2 className='text-center text-title font-medium text-primary'>
+          {t('services_howDoWeWork_title')}
         </h2>
         <p className='text-center text-paragraph leading-relaxed lg:px-52'>
-          We efficiently combine all required datasets into a complex problem
-          and solve it mathematically in minutes. Here{"'"}s a brief overview of
-          our process:
+          {t('services_howDoWeWork_paragraph')}
         </p>
       </div>
 
       <section className='flex justify-center'>
         <div className='flex max-w-[1400px] flex-col items-center justify-between gap-8 md:flex-row lg:justify-evenly '>
           {/* content */}
-          <div className='flex flex-col gap-4 px-6 md:ml-[100px] md:w-1/2 md:px-0 '>
+          <div className='flex flex-col gap-8 px-6 md:ml-[40px] md:w-1/2 md:px-0 lg:ml-[80px] '>
             <h3 className='text-subtitle font-semibold text-primary'>
-              Once we generate the optimal design configuration we also provide:
+              {t('services_howDoWeWork_sideSubtitle')}
             </h3>
-            <ul className='flex list-inside list-disc flex-col gap-4'>
+            <ul className='flex list-inside list-disc flex-col gap-8'>
               <li className='text-paragraph'>
-                <strong>Review and Feedback:</strong> Present the optimized
-                solutions to the client, incorporating their feedback and making
-                adjustments as needed to ensure alignment with their goals.
+                <Trans
+                  i18nKey={'services_howDoWeWork_sideFirstParagraph'}
+                  components={{
+                    1: <strong></strong>
+                  }}
+                />
               </li>
               <li className='text-paragraph'>
-                <strong>Implementation Support:</strong> Offer comprehensive
-                guidance and support during the implementation phase, ensuring
-                seamless integration and maximizing project efficiency.
+                <Trans
+                  i18nKey={'services_howDoWeWork_sideSecondParagraph'}
+                  components={{
+                    1: <strong></strong>
+                  }}
+                />
               </li>
             </ul>
           </div>
 
           {/* timeline */}
-          <div className='w-1/2'>
+          <div className='px-6 md:w-1/2'>
             <div className='relative flex pb-12'>
-              <div className='absolute inset-0 flex h-full w-10 items-center justify-center'>
+              <div className='absolute inset-0  left-2 flex h-full w-10 items-center justify-center'>
                 <div className='pointer-events-none h-full w-1 bg-gray-200' />
               </div>
-              <div className='relative inline-flex size-10 flex-shrink-0 items-center justify-center rounded-full bg-lightGray text-white'>
-                <PiSolarPanel size={26} />
+              <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-lightGray text-white'>
+                <PiSolarPanel size={30} />
               </div>
               <div className='flex-grow pl-4'>
                 <h2 className='mb-1 text-paragraph text-gray-900'>
-                  Input Parameters (project specifics)
+                  {t('services_howDoWeWork_Icon1')}
                 </h2>
-                {/* <p className='leading-relaxed'>
-                VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
-                bespoke try-hard cliche palo santo offal.
-              </p> */}
               </div>
             </div>
             <div className='relative flex pb-12'>
-              <div className='absolute inset-0 flex h-full w-10 items-center justify-center'>
+              <div className='absolute inset-0  left-2 flex h-full w-10 items-center justify-center'>
                 <div className='pointer-events-none h-full w-1 bg-gray-200' />
               </div>
-              <div className='relative inline-flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-primary text-white'>
-                <FaCheckDouble size={24} />
+              <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-primary text-white'>
+                <FaCheckDouble size={30} />
               </div>
               <div className='flex-grow pl-4'>
                 <h2 className='mb-1 text-paragraph text-gray-900'>
-                  Data Check by expert
+                  {t('services_howDoWeWork_Icon2')}
                 </h2>
-                {/* <p className='leading-relaxed'>
-                Vice migas literally kitsch +1 pok pok. Truffaut hot chicken
-                slow-carb health goth, vape typewriter.
-              </p> */}
               </div>
             </div>
             <div className='relative flex pb-12'>
-              <div className='absolute inset-0 flex h-full w-10 items-center justify-center'>
+              <div className='absolute inset-0  left-2 flex h-full w-10 items-center justify-center'>
                 <div className='pointer-events-none h-full w-1 bg-gray-200' />
               </div>
-              <div className='relative inline-flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-primary text-white'>
-                <BsDatabaseGear size={25} />
+              <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-primary text-white'>
+                <BsDatabaseGear size={30} />
               </div>
               <div className='flex-grow pl-4'>
                 <h2 className='mb-1 text-paragraph text-gray-900'>
-                  Complex Modeling
+                  {t('services_howDoWeWork_Icon3')}
                 </h2>
-                {/* <p className='leading-relaxed'>
-                Coloring book nar whal glossier master cleanse umami. Salvia +1
-                master cleanse blog taiyaki.
-              </p> */}
               </div>
             </div>
             <div className='relative flex pb-12'>
-              <div className='absolute inset-0 flex h-full w-10 items-center justify-center'>
+              <div className='absolute inset-0  left-2 flex h-full w-10 items-center justify-center'>
                 <div className='pointer-events-none h-full w-1 bg-gray-200' />
               </div>
-              <div className='relative inline-flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-primary text-white'>
-                <LiaSearchSolid size={25} />
+              <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-primary text-white'>
+                <LiaSearchSolid size={30} />
               </div>
               <div className='flex-grow pl-4'>
                 <h2 className='mb-1 text-paragraph text-gray-900'>
-                  Solution (client{"'"}s requirements)
+                  {t('services_howDoWeWork_Icon4')}
                 </h2>
-                {/* <p className='leading-relaxed'>
-                VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
-                bespoke try-hard cliche palo santo offal.
-              </p> */}
               </div>
             </div>
             <div className='relative flex'>
-              <div className='relative inline-flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-lightGray text-white'>
-                <FaRegHandshake size={25} />
+              <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-lightGray text-white'>
+                <FaRegHandshake size={30} />
               </div>
               <div className='flex-grow pl-4'>
                 <h2 className='mb-1 text-paragraph text-gray-900'>
-                  Financial Closure/FID
+                  {t('services_howDoWeWork_Icon5')}
                 </h2>
-                {/* <p className='leading-relaxed'>
-                Pitchfork ugh tattooed scenester echo park gastropub whatever
-                cold-pressed retro.
-              </p> */}
               </div>
             </div>
           </div>
