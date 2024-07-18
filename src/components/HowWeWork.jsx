c8a6cb949a0cca49b3840f58e3d1dde73b6253c9
LuisMFG <luismiguelfloresgr@gmail.com>
2024-07-18 17:59:19 +0200

    finished

--- src/components/HowWeWork.jsx
+++ src/components/HowWeWork.jsx
@@ -22,95 +22,101 @@ const HowWeWork = () => {
         </p>
       </div>
 
-      <section className='flex justify-center'>
-        <div className='flex max-w-[1400px] flex-col items-center justify-between gap-8 md:flex-row lg:justify-evenly '>
-          {/* content */}
-          <div className='flex flex-col gap-8 px-6 md:ml-[40px] md:w-1/2 md:px-0 lg:ml-[80px] '>
-            <h3 className='text-subtitle font-semibold text-primary'>
-              {t('services_howDoWeWork_sideSubtitle')}
-            </h3>
-            <ul className='flex  flex-col gap-8'>
-              <li className='text-paragraph'>
-                <Trans
-                  i18nKey={'services_howDoWeWork_sideFirstParagraph'}
-                  components={{
-                    1: <strong></strong>
-                  }}
-                />
-              </li>
-              <li className='text-paragraph'>
-                <Trans
-                  i18nKey={'services_howDoWeWork_sideSecondParagraph'}
-                  components={{
-                    1: <strong></strong>
-                  }}
-                />
-              </li>
-            </ul>
+      <section className='flex items-center justify-center'>
+        <div className='flex max-w-[1500px] flex-col justify-center gap-10 md:flex-row md:gap-0'>
+          {/* timeline */}
+          <div className='flex items-center justify-center md:w-1/2 md:px-10'>
+            <div className='flex flex-col justify-center px-6 md:px-0'>
+              <div className='relative flex pb-12'>
+                <div className='absolute inset-0  left-2 flex h-full w-10 items-center justify-center'>
+                  <div className='pointer-events-none h-full w-1 bg-gray-200' />
+                </div>
+                <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-lightGray text-white'>
+                  <PiSolarPanel size={30} />
+                </div>
+                <div className='flex-grow pl-4'>
+                  <h2 className='mb-1 text-subtitle font-semibold text-primary'>
+                    {t('services_howDoWeWork_Icon1')}
+                  </h2>
+                </div>
+              </div>
+              <div className='relative flex pb-12'>
+                <div className='absolute inset-0  left-2 flex h-full w-10 items-center justify-center'>
+                  <div className='pointer-events-none h-full w-1 bg-gray-200' />
+                </div>
+                <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-primary text-white'>
+                  <FaCheckDouble size={30} />
+                </div>
+                <div className='flex-grow pl-4'>
+                  <h2 className='mb-1 text-subtitle font-semibold text-primary'>
+                    {t('services_howDoWeWork_Icon2')}
+                  </h2>
+                </div>
+              </div>
+              <div className='relative flex pb-12'>
+                <div className='absolute inset-0 left-2 flex h-full w-10 items-center justify-center'>
+                  <div className='pointer-events-none h-full w-1 bg-gray-200' />
+                </div>
+                <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-primary text-white'>
+                  <BsDatabaseGear size={30} />
+                </div>
+                <div className='flex-grow pl-4'>
+                  <h2 className='mb-1 text-subtitle font-semibold text-primary'>
+                    {t('services_howDoWeWork_Icon3')}
+                  </h2>
+                </div>
+              </div>
+              <div className='relative flex pb-12'>
+                <div className='absolute inset-0  left-2 flex h-full w-10 items-center justify-center'>
+                  <div className='pointer-events-none h-full w-1 bg-gray-200' />
+                </div>
+                <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-primary text-white'>
+                  <LiaSearchSolid size={30} />
+                </div>
+                <div className='flex-grow pl-4'>
+                  <h2 className='mb-1 text-subtitle font-semibold text-primary'>
+                    {t('services_howDoWeWork_Icon4')}
+                  </h2>
+                </div>
+              </div>
+              <div className='relative flex'>
+                <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-lightGray text-white'>
+                  <FaRegHandshake size={30} />
+                </div>
+                <div className='flex-grow pl-4'>
+                  <h2 className='mb-1 text-subtitle font-semibold text-primary'>
+                    {t('services_howDoWeWork_Icon5')}
+                  </h2>
+                </div>
+              </div>
+            </div>
           </div>
 
-          {/* timeline */}
-          <div className='px-6 md:w-1/2'>
-            <div className='relative flex pb-12'>
-              <div className='absolute inset-0  left-2 flex h-full w-10 items-center justify-center'>
-                <div className='pointer-events-none h-full w-1 bg-gray-200' />
-              </div>
-              <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-lightGray text-white'>
-                <PiSolarPanel size={30} />
-              </div>
-              <div className='flex-grow pl-4'>
-                <h2 className='mb-1 text-subtitle font-semibold text-primary'>
-                  {t('services_howDoWeWork_Icon1')}
-                </h2>
-              </div>
-            </div>
-            <div className='relative flex pb-12'>
-              <div className='absolute inset-0  left-2 flex h-full w-10 items-center justify-center'>
-                <div className='pointer-events-none h-full w-1 bg-gray-200' />
-              </div>
-              <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-primary text-white'>
-                <FaCheckDouble size={30} />
-              </div>
-              <div className='flex-grow pl-4'>
-                <h2 className='mb-1 text-subtitle font-semibold text-primary'>
-                  {t('services_howDoWeWork_Icon2')}
-                </h2>
-              </div>
-            </div>
-            <div className='relative flex pb-12'>
-              <div className='absolute inset-0 left-2 flex h-full w-10 items-center justify-center'>
-                <div className='pointer-events-none h-full w-1 bg-gray-200' />
-              </div>
-              <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-primary text-white'>
-                <BsDatabaseGear size={30} />
-              </div>
-              <div className='flex-grow pl-4'>
-                <h2 className='mb-1 text-subtitle font-semibold text-primary'>
-                  {t('services_howDoWeWork_Icon3')}
-                </h2>
-              </div>
-            </div>
-            <div className='relative flex pb-12'>
-              <div className='absolute inset-0  left-2 flex h-full w-10 items-center justify-center'>
-                <div className='pointer-events-none h-full w-1 bg-gray-200' />
-              </div>
-              <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-primary text-white'>
-                <LiaSearchSolid size={30} />
-              </div>
-              <div className='flex-grow pl-4'>
-                <h2 className='mb-1 text-subtitle font-semibold text-primary'>
-                  {t('services_howDoWeWork_Icon4')}
-                </h2>
-              </div>
-            </div>
-            <div className='relative flex'>
-              <div className='relative inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-lightGray text-white'>
-                <FaRegHandshake size={30} />
-              </div>
-              <div className='flex-grow pl-4'>
-                <h2 className='mb-1 text-subtitle font-semibold text-primary'>
-                  {t('services_howDoWeWork_Icon5')}
-                </h2>
+          {/* content */}
+          <div className='flex items-center justify-center md:w-1/2'>
+            <div className='flex flex-col items-center justify-center gap-8 md:flex-row'>
+              <div className='flex flex-col gap-8 px-6 md:ml-[40px]  md:px-0 lg:ml-[80px] '>
+                <h3 className='text-center text-subtitle font-semibold text-primary md:text-start'>
+                  {t('services_howDoWeWork_sideSubtitle')}
+                </h3>
+                <ul className='flex  flex-col gap-8 text-center md:text-start'>
+                  <li className='text-paragraph'>
+                    <Trans
+                      i18nKey={'services_howDoWeWork_sideFirstParagraph'}
+                      components={{
+                        1: <strong></strong>
+                      }}
+                    />
+                  </li>
+                  <li className='text-paragraph'>
+                    <Trans
+                      i18nKey={'services_howDoWeWork_sideSecondParagraph'}
+                      components={{
+                        1: <strong></strong>
+                      }}
+                    />
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
