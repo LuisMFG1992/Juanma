7181c073996d8f6cc76223e55ba1212b543f1219
LuisMFG <luismiguelfloresgr@gmail.com>
2024-08-12 19:28:49 +0200

    news pic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,14 +7,25 @@ const Card = () => {
   return (
     <Link
       to={'https://energiaestrategica.es/fidgate-ofrece-soluciones/'}
-      className='flex h-[320px] w-[350px] flex-col gap-4 rounded-xl shadow-lg'
+      className='flex h-[320px] w-[300px] flex-col gap-4 rounded-xl shadow-lg'
     >
       <img
         src={newImage}
         alt='news image'
         className='rounded-t-xl bg-blue-900 object-cover'
       />
-      <p className='px-4 text-lg'>{t('home_news_title')}</p>
+      <div className='flex  flex-col gap-2 px-4'>
+        <p className='text-lg font-semibold text-primary'>
+          {t('home_news_title')}
+        </p>
+        <p className=' text-lg'>{t('home_news_text')}</p>
+        <div className='flex justify-between'>
+          <p className='cursor-pointer text-lg text-primary underline'>
+            {t('home_news_button')}
+          </p>
+          <p className='text-lg'>July 30, 2024</p>
+        </div>
+      </div>
     </Link>
   )
 }
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,7 +3,7 @@ import Card from './Card'
 const News = () => {
   return (
     <div className='flex flex-col items-center justify-center gap-4 pt-12'>
-      <p className='text-subtitle font-semibold text-primary'>
+      <p className='text-center text-subtitle font-semibold text-primary'>
         Read the latest news about Fidgate:
       </p>
       <Card />
