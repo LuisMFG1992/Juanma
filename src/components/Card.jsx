80cbf487ea475299d33ec44af343eb4e7794f351
LuisMFG <luismiguelfloresgr@gmail.com>
2024-09-05 21:44:55 +0200

    news2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,33 @@
 import { useTranslation } from 'react-i18next'
-import newImage from '../assets/new.jpg'
 import { Link } from 'react-router-dom'
 
-const Card = () => {
+const Card = ({
+  image = '',
+  title = 'Title',
+  company = 'Company name',
+  link = '#',
+  date = 'DDMMYYY'
+}) => {
   const { t } = useTranslation()
+
   return (
     <Link
-      to={'https://energiaestrategica.es/fidgate-ofrece-soluciones/'}
-      className='flex h-[320px] w-[300px] flex-col gap-4 rounded-xl shadow-lg'
+      to={link}
+      className='flex h-[320px] w-[300px] flex-col rounded-xl shadow-lg'
     >
       <img
-        src={newImage}
+        src={image}
         alt='news image'
-        className='rounded-t-xl bg-blue-900 object-cover'
+        className='h-[160px] w-[300px] rounded-t-xl bg-blue-900 object-cover'
       />
-      <div className='flex  flex-col gap-2 px-4'>
-        <p className='text-lg font-semibold text-primary'>
-          {t('home_news_title')}
-        </p>
-        <p className=' text-lg'>{t('home_news_text')}</p>
+      <div className='flex flex-1 flex-col justify-between gap-2 p-4'>
+        <p className='text-lg font-semibold text-primary'>{title}</p>
+        <p className=' text-lg'>{company}</p>
         <div className='flex justify-between'>
           <p className='cursor-pointer text-lg text-primary underline'>
             {t('home_news_button')}
           </p>
-          <p className='text-lg'>July 30, 2024</p>
+          <p className='text-lg'>{date}</p>
         </div>
       </div>
     </Link>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,25 @@
 import Card from './Card'
+import new1 from '../assets/new.jpg'
+import new2 from '../assets/new2.jpg'
+
+const news = [
+  {
+    id: 1,
+    image: new1,
+    title: 'Technical and financial optimization.',
+    company: 'Energía Estratégica',
+    link: 'https://energiaestrategica.es/fidgate-ofrece-soluciones/',
+    date: '30/07/2024'
+  },
+  {
+    id: 2,
+    image: new2,
+    title: 'Maximizing Solar Project Value.',
+    company: 'HVH',
+    link: 'https://hidrogenoverdehoy.com.ar/el-camino-a-fidgate-soluciones-innovadoras-para-maximizar-el-valor-de-los-proyectos-solares-fotovoltaicos/',
+    date: '05/09/2024'
+  }
+]
 
 const News = () => {
   return (
@@ -6,7 +27,18 @@ const News = () => {
       <p className='text-center text-subtitle font-semibold text-primary'>
         Read the latest news about FIDgate:
       </p>
-      <Card />
+      <div className='flex gap-8'>
+        {news.map((element) => (
+          <Card
+            key={element.id}
+            image={element.image}
+            title={element.title}
+            company={element.company}
+            link={element.link}
+            date={element.date}
+          />
+        ))}
+      </div>
     </div>
   )
 }
