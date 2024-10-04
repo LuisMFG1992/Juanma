47cab7e86da11f2bbe332030b85e0ec7fd6fa9e2
LuisMFG1992 <luismiguelfloresgr@gmail.com>
2024-10-04 22:00:10 +0200

    horizonal gap teams

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -20,58 +20,12 @@ const OurTeam = () => {
             {t('about_ourKeyTeamMembers_paragraph')}
           </p>
         </div>
-        <div className='grid gap-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 '>
-          {/* <div className='p-4 '> */}
+        <div className='grid gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5'>
           {teamMembers.map((member, idx) => (
             <Member key={idx} member={member} />
-            //   <p key={idx}>hey</p>
           ))}
-          {/* <Member /> */}
-          {/* Member */}
-          {/* <div className='flex h-full flex-col items-center text-center'>
-              <img
-                alt='team'
-                className='mb-4 w-full rounded-lg object-cover object-center'
-                src='https://dummyimage.com/200x200'
-              />
-              <div className='flex w-full flex-col gap-1'>
-                <h2 className='text-paragraph font-medium text-gray-900'>
-                  Juan Manuel Roldan
-                </h2>
-                <div className='flex items-center justify-center gap-2'>
-                  <h3 className='text-lg text-gray-500'>CEO & Founder</h3>
-                  <Link to={''}>
-                    <FaLinkedin className='text-2xl ' />
-                  </Link>
-                </div>
-
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button variant='link' className='text-md h-auto p-0'>
-                      Open Bio
-                      <FaArrowRight className='ml-2' />
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className='w-80'>
-                    <p>
-                      MSc. Mechanical Engineering (UNMDP) & Certified Energy
-                      Expert (RENAC)
-                    </p>
-                    <br />
-                    <p>
-                      Optimization expert with over 7 years of experience in the
-                      renewable energy industry. He has helped bridge the gap
-                      between finance and engineering for some of the sector
-                      {"'"}s largest investors, including Copenhagen
-                      Infrastructure Partners and European Energy.
-                    </p>
-                  </PopoverContent>
-                </Popover>
-              </div>
-            </div> */}
         </div>
       </div>
-      {/* </div> */}
     </section>
   )
 }
